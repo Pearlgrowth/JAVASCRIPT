fe20c92a07b0bece12222923ca43b9b6e5d84c6c
mepearl <139564822+Pearlgrowth@users.noreply.github.com>
2024-01-16 16:46:52 +0300

    first commit

---
+++ exercise_question3.js
@@ -0,0 +1,12 @@
+// // Output of the first line
+// One. this is because the string passed onto it is One
+
+
+
+// Output of second line:
+// String One. The value passed to the object is String One.
+
+
+
+// Output of third line:
+// true. Because they are the same value and also of the same type.
